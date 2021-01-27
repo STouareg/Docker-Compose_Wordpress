0ae0b51a1e06c50ff9c2cf888099c279ca68d240
s.turyk <sergii.turyk@gmail.com>
2021-01-27 21:42:20 +0200

    fix ssh script

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -3,6 +3,6 @@
 COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
-cd /home/admturyk/wordpress-training/
+cd //home/admturyk/lesson2_wordpress_docker-compose/
 $COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
